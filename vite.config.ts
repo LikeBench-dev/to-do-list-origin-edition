034fc990235104692e8c63862cefbe5c98e446cd
Роман Березань <frolals@gmail.com>
2025-12-03 16:34:59 +0300

    Closes #2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -18,4 +17,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  base: process.env.NODE_ENV === 'production'
+    ? '/to-do-list-origin-edition/'  
+    : '/'
 })
